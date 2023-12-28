d80f30b7df9c6d49ff3f6f5491756e4c6e0c4639
Giorgi <giorgi.abashidze2@gmail.com>
2023-12-28 17:13:23 +0400

    feat(MBA-4): add transaction model and migration file

--- src/apps/transactions/models.py
+++ src/apps/transactions/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+from apps.account.models import Account
+from apps.base.models import BaseModel
+
+
+class TransactionStatusChoice(models.TextChoices):
+    INITIATED = "INITIATED"
+    PENDING = "PENDING"
+    COMPLETED = "COMPLETED"
+    FAILED = "FAILED"
+    ERROR = "ERROR"
+
+
+class TransactionTypeChoice(models.TextChoices):
+    TRANSFER = "transfer"
+    WITHDRAWAL = "withdrawal"
+    DEPOSIT = "deposit"
+
+
+class Transaction(BaseModel):
+    """Model for Transactions"""
+
+    sender = models.ForeignKey(
+        Account, on_delete=models.CASCADE, related_name="sent_transactions"
+    )
+    receiver = models.ForeignKey(
+        Account, on_delete=models.CASCADE, related_name="received_transactions"
+    )
+    amount = models.DecimalField(decimal_places=2, max_digits=20)
+    status = models.CharField(choices=TransactionStatusChoice.choices)
+    type = models.CharField(choices=TransactionTypeChoice.choices)
+
+    def __str__(self):
+        return self.id
