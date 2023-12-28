86bd079d3d81790f87a0f476dd9c3d67a85c2558
TATO <tatososelia93@gmail.com>
2023-12-28 15:22:40 +0400

    feat(MBA-3): add account model and migration file

---
+++ src/apps/account/models.py
@@ -0,0 +1,21 @@
+"""account model."""
+from django.db import models
+
+from apps.base.models import BaseModel
+from apps.profiles.models import Profile
+
+
+class AccountTypeChoice(models.TextChoices):
+    DEPOSIT = "deposit"
+    CREDIT = "credit"
+    DEBIT = "debit"
+    CONSUMER = "consumer"
+
+
+class Account(BaseModel):
+    """Account model."""
+
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    account_number = models.CharField(max_length=255, unique=True)
+    account_type = models.CharField(choices=AccountTypeChoice.choices)
+    balance = models.DecimalField(decimal_places=2, max_digits=20)
