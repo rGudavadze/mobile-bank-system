4efcd43b0bfed08a9d58554fe2db210ce078e00e
Qildeli <anaqildeli@gmail.com>
2023-12-29 17:38:00 +0400

    feat(MBA-6): add register endpoint for user

--- src/apps/users/views.py
+++ src/apps/users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from apps.users.serializers import UserSerializer
+
+
+class UserRegisterAPI(APIView):
+    """
+    An endpoint to create a new user.
+    """
+
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
