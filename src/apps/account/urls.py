de6ad8f75b03f25ec6830fe0c09cce71136dcdda
NikaMdinaradze <mdinaradzenika04@gmail.com>
2023-12-29 18:53:14 +0400

    MB-8 Add account detail class

--- src/apps/account/urls.py
+++ src/apps/account/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
-from .views import AccountList
+from .views import AccountDetail, AccountList
 
-urlpatterns = [path("", AccountList.as_view(), name="account-list")]
+urlpatterns = [
+    path("", AccountList.as_view(), name="todo-list"),
+    path("<int:pk>", AccountDetail.as_view(), name="todo-details"),
+]
--- src/apps/account/views.py
+++ src/apps/account/views.py
@@ -7,3 +7,8 @@ from .serializers import AccountSerializer
 class AccountList(generics.ListCreateAPIView):
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
+
+
+class AccountDetail(generics.RetrieveDestroyAPIView):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
