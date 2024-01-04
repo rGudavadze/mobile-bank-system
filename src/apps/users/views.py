4bf50dd663b6ff6aaea8e5828283a9ff707c66be
Qildeli <anaqildeli@gmail.com>
2024-01-04 17:26:32 +0400

    feat(MBA-6): add TokenRefreshView for JWT access token renewal using refresh token

--- src/apps/users/views.py
+++ src/apps/users/views.py
@@ -7,7 +7,11 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from apps.users.serializers import AuthTokenSerializer, UserSerializer
+from apps.users.serializers import (
+    AuthTokenSerializer,
+    RefreshTokenSerializer,
+    UserSerializer,
+)
 
 
 class UserRegisterAPI(APIView):
@@ -52,4 +56,41 @@ class TokenObtainView(APIView):
                 {"access_token": access_token, "refresh_token": refresh_token},
                 status=status.HTTP_200_OK,
             )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TokenRefreshView(APIView):
+    """
+    An endpoint to refresh JWT access tokens using a refresh token.
+    """
+
+    serializer_class = RefreshTokenSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            try:
+                refresh_token = serializer.validated_data.get("refresh_token")
+                payload = jwt.decode(
+                    refresh_token, settings.SECRET_KEY, algorithms=["HS256"]
+                )
+                user_id = payload["user_id"]
+                new_access_token = jwt.encode(
+                    {"user_id": user_id, "exp": datetime.now() + timedelta(minutes=5)},
+                    settings.SECRET_KEY,
+                    algorithm="HS256",
+                )
+                return Response(
+                    {"access_token": new_access_token}, status=status.HTTP_200_OK
+                )
+            except jwt.ExpiredSignatureError:
+                return Response(
+                    {"error": "Refresh token expired"},
+                    status=status.HTTP_401_UNAUTHORIZED,
+                )
+            except jwt.InvalidTokenError:
+                return Response(
+                    {"error": "Invalid token"}, status=status.HTTP_401_UNAUTHORIZED
+                )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
