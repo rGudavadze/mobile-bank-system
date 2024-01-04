c9ed0a7d0b99734f23e899af2f802b8e31f4f9dd
Qildeli <anaqildeli@gmail.com>
2024-01-04 17:29:44 +0400

    feat(MBA-6): add URL path for TokenRefreshView to enable access token renewal

--- src/apps/users/urls.py
+++ src/apps/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
 from apps.users import views
-from apps.users.views import TokenObtainView
+from apps.users.views import TokenObtainView, TokenRefreshView
 
 urlpatterns = [
     path("user/register/", views.UserRegisterAPI.as_view(), name="user_register"),
     path("token/", TokenObtainView.as_view(), name="token_obtain"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
