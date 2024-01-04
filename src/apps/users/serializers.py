b11017cd4b0e1b64e63a0e289a2c27cd6cef4749
Qildeli <anaqildeli@gmail.com>
2024-01-04 17:19:28 +0400

    feat(MBA-6): add AuthTokenSerializer for user authentication via email and password

--- src/apps/users/serializers.py
+++ src/apps/users/serializers.py
@@ -1,14 +1,26 @@
+from django.contrib.auth import authenticate, get_user_model
 from rest_framework import serializers
-
-from apps.users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ("id", "email", "password", "created_at")
         read_only_fields = ("id", "created_at")
         extra_kwargs = {"password": {"write_only": True, "min_length": 6}}
 
-        def create(self, validated_data):
-            return User.objects.create_user(**validated_data)
+    def create(self, validated_data):
+        return get_user_model().objects.create_user(**validated_data)
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(style={"input_type": "password"})
+
+    def validate(self, data):
+        user = authenticate(email=data.get("email"), password=data.get("password"))
+        if not user:
+            raise serializers.ValidationError("Unable to log in.")
+        if not user.is_active:
+            raise serializers.ValidationError("Account is inactive.")
+        return user
