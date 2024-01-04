77a8466db4fbd3ca24bd61e38e152185368fe77d
Qildeli <anaqildeli@gmail.com>
2024-01-04 17:23:19 +0400

    feat(MBA-6): add URL path for TokenObtainView to handle token requests

--- src/apps/users/urls.py
+++ src/apps/users/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
 from apps.users import views
+from apps.users.views import TokenObtainView
 
-urlpatterns = [path("register/", views.UserRegisterAPI.as_view(), name="user_register")]
+urlpatterns = [
+    path("user/register/", views.UserRegisterAPI.as_view(), name="user_register"),
+    path("token/", TokenObtainView.as_view(), name="token_obtain"),
+]
