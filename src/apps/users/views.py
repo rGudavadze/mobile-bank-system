bc23008bfdbfb60f9916baa792230e50984455a9
Qildeli <anaqildeli@gmail.com>
2024-01-04 17:21:38 +0400

    feat(MBA-6): add TokenObtainView for JWT access and refresh token generation

--- src/apps/users/views.py
+++ src/apps/users/views.py
@@ -1,9 +1,13 @@
+from datetime import datetime, timedelta
+
+import jwt
+from django.conf import settings
 from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from apps.users.serializers import UserSerializer
+from apps.users.serializers import AuthTokenSerializer, UserSerializer
 
 
 class UserRegisterAPI(APIView):
@@ -19,4 +23,33 @@ class UserRegisterAPI(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TokenObtainView(APIView):
+    """
+    An endpoint to obtain JWT access and refresh tokens for a user.
+    """
+
+    serializer_class = AuthTokenSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data
+            access_token = jwt.encode(
+                {"user_id": str(user.id), "exp": datetime.now() + timedelta(minutes=5)},
+                settings.SECRET_KEY,
+                algorithm="HS256",
+            )
+            refresh_token = jwt.encode(
+                {"user_id": str(user.id), "exp": datetime.now() + timedelta(days=1)},
+                settings.SECRET_KEY,
+                algorithm="HS256",
+            )
+            return Response(
+                {"access_token": access_token, "refresh_token": refresh_token},
+                status=status.HTTP_200_OK,
+            )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
