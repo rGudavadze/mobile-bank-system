2c2a272001aad780dda755539a834d50d340f41b
TATO <tatososelia93@gmail.com>
2024-01-05 11:58:53 +0400

    feat(MBA-7): add serializer for transaction model

---
+++ src/apps/transactions/serializers.py
@@ -0,0 +1,12 @@
+"""Serializers for the transactions model."""
+from rest_framework import serializers
+
+from src.apps.transactions.models import Transaction
+
+
+class TransactionSerializer(serializers.ModelSerializer):
+    """Serializer for transaction model."""
+    class Meta:
+        model = Transaction
+        fields = ('id', 'sender', 'receiver', 'amount', 'status', 'type')
+        read_only_fields = ('id', 'status')
