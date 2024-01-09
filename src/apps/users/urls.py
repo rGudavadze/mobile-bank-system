0b2c464633fff4969196af0a982af6a15e782255
Qildeli <anaqildeli@gmail.com>
2024-01-09 18:50:08 +0400

    feat(MBA-6): Add password management endpoints in urls.py for reset and forget functionality

--- src/apps/users/urls.py
+++ src/apps/users/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
 
 from apps.users import views
-from apps.users.views import TokenObtainView, TokenRefreshView
+from apps.users.views import (
+    PasswordForgetView,
+    PasswordResetView,
+    TokenObtainView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
-    path("user/register/", views.UserRegisterAPI.as_view(), name="user_register"),
-    path("token/", TokenObtainView.as_view(), name="token_obtain"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    # Endpoint for user registration
+    path("user/register/", views.UserRegisterAPI.as_view(), name="user-register"),
+    # Endpoints for JWT token management
+    path("token/", TokenObtainView.as_view(), name="token-obtain"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
+    # Password management endpoints
+    path("user/password-forget/", PasswordForgetView.as_view(), name="password-forget"),
+    path("user/password-reset/", PasswordResetView.as_view(), name="password-reset"),
 ]
