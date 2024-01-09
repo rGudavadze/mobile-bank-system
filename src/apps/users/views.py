794bc23c4af2e0afc0332376e8559b0ec4bdd24c
Qildeli <anaqildeli@gmail.com>
2024-01-09 18:45:17 +0400

    feat(MBA-6): Add password forget and reset API views in authentication module

--- src/apps/users/views.py
+++ src/apps/users/views.py
@@ -2,14 +2,19 @@ from datetime import datetime, timedelta
 
 import jwt
 from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.core.mail import send_mail
+from django.urls import reverse
 from rest_framework import status
 from rest_framework.exceptions import AuthenticationFailed
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from apps.users.serializers import (
     AuthTokenSerializer,
+    PasswordForgetSerializer,
+    PasswordResetSerializer,
     RefreshTokenSerializer,
     UserSerializer,
 )
@@ -21,14 +26,13 @@ class UserRegisterAPI(APIView):
     """
 
     serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class TokenObtainView(APIView):
@@ -98,4 +102,91 @@ class TokenRefreshView(APIView):
                 return Response(
                     {"error": "Invalid token"}, status=status.HTTP_401_UNAUTHORIZED
                 )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PasswordForgetView(APIView):
+    """
+    An endpoint for user to send POST request to reset password.
+    """
+
+    serializer_class = PasswordForgetSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            email = serializer.validated_data.get("email")
+            try:
+                user = get_user_model().objects.get(email=email)
+            except get_user_model().DoesNotExist:
+                return Response(
+                    {"error": "User with this email does not exist"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+            reset_token = jwt.encode(
+                {"user_id": str(user.id), "exp": datetime.now() + timedelta(minutes=5)},
+                settings.SECRET_KEY,
+                algorithm="HS256",
+            )
+
+            password_forget_url = (
+                request.build_absolute_uri(reverse("password-forget"))
+                + f"?token={reset_token}"
+            )
+
+            # Send email
+            subject = "Password Reset Request"
+            message = f"Hi, click on the link to reset password:\n{password_forget_url}"
+            email_from = settings.EMAIL_HOST_USER
+            recipient_list = [email]
+
+            send_mail(subject, message, email_from, recipient_list, fail_silently=False)
+
+            return Response(
+                "Password reset e-mail has been sent.", status=status.HTTP_200_OK
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PasswordResetView(APIView):
+    """
+    An endpoint for user to send POST request to make new password
+    """
+
+    serializer_class = PasswordResetSerializer
+
+    def patch(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            reset_token = serializer.validated_data["reset_token"]
+            new_password = serializer.validated_data["new_password"]
+            try:
+                payload = jwt.decode(
+                    reset_token, settings.SECRET_KEY, algorithms=["HS256"]
+                )
+                user_id = payload["user_id"]
+                if user_id is None:
+                    raise jwt.InvalidTokenError
+
+                user = get_user_model().objects.get(id=user_id)
+                user.set_password(new_password)
+                user.save()
+                return Response(
+                    {"message": "Password has been reset successfully."},
+                    status=status.HTTP_200_OK,
+                )
+
+            except jwt.ExpiredSignatureError:
+                return Response(
+                    {"error": "Reset token has expired."},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            except (jwt.InvalidTokenError, get_user_model().DoesNotExist):
+                return Response(
+                    {"error": "Invalid token or user does not exist."},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
