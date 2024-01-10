0423b38315ef31068e02bbe3547ec2bedef0c553
Qildeli <anaqildeli@gmail.com>
2024-01-10 15:08:05 +0400

    refactor(MBA-6): Add decode_refresh_token in jwt_authentication.py for cleaner layer

--- src/core/jwt_authentication.py
+++ src/core/jwt_authentication.py
@@ -1,27 +1,29 @@
 import jwt
-from django.conf import settings
 from django.contrib.auth import get_user_model
 from rest_framework import exceptions
 from rest_framework.authentication import TokenAuthentication
+
+from apps.users.jwt_utils import decode_refresh_token
 
 
 class JWTAuthentication(TokenAuthentication):
     def authenticate(self, request):
         # Extract the JWT from the Authorization header
         jwt_token = request.META.get("HTTP_AUTHORIZATION")
+        print(jwt_token)
         if jwt_token is None:
             return None
 
         # Decode the JWT and verify its signature
         try:
-            payload = jwt.decode(jwt_token, settings.SECRET_KEY, algorithms=["HS256"])
+            payload = decode_refresh_token(jwt_token)
         except jwt.exceptions.InvalidSignatureError:
             raise exceptions.AuthenticationFailed("Invalid signature")
         except jwt.exceptions.DecodeError:
             raise exceptions.AuthenticationFailed("Invalid token")
 
         # Get the user from the database
-        user_id = payload["user_id"]
+        user_id = payload.get("user_id")
         if user_id is None:
             raise exceptions.AuthenticationFailed("User identifier not found in JWT")
 
