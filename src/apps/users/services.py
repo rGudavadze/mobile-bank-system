522d70313f1ba1b5e91ecdf1b63ade90a2623e7a
Qildeli <anaqildeli@gmail.com>
2024-01-10 15:47:20 +0400

    refactor(MBA-6): Move send_password_reset_email logic to services.py for cleaner view layer

--- src/apps/users/services.py
+++ src/apps/users/services.py
@@ -1,10 +1,15 @@
 import jwt
+from django.conf import settings
+from django.core.mail import send_mail
 from rest_framework.exceptions import AuthenticationFailed
 
 from apps.users.jwt_utils import decode_refresh_token, generate_access_token
 
 
 def update_access_token(refresh_token):
+    """
+    Updates access token by decoding JWT refresh token.
+    """
     try:
         payload = decode_refresh_token(refresh_token)
         user_id = payload.get("user_id")
@@ -21,3 +26,15 @@ def update_access_token(refresh_token):
 
     except jwt.InvalidTokenError as e:
         raise AuthenticationFailed("Invalid token") from e
+
+
+def send_password_reset_email(email, password_forget_url):
+    """
+    Sends a password reset email to the specified email address.
+    """
+    subject = "Password Reset Request"
+    message = f"Hi, click on the link to reset password:\n{password_forget_url}"
+    email_from = settings.EMAIL_HOST_USER
+    recipient_list = [email]
+
+    send_mail(subject, message, email_from, recipient_list, fail_silently=False)
