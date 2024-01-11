6b49399690dd7c06413ea999866cddbb08fdc10a
Qildeli <anaqildeli@gmail.com>
2024-01-11 17:36:18 +0400

    docs(MBA-6): Add docstrings to jwt_utils module functions

--- src/apps/users/jwt_utils.py
+++ src/apps/users/jwt_utils.py
@@ -1,3 +1,8 @@
+"""
+Module provides utility functions for generating and decoding JSON Web Tokens (JWTs)
+for user authentication. It includes functions to create both access tokens and
+refresh tokens, as well as a function to decode a given refresh token.
+"""
 from datetime import datetime, timedelta
 
 import jwt
@@ -5,6 +10,10 @@ from django.conf import settings
 
 
 def generate_access_token(user_id):
+    """
+    Generates a JWT access token with a short expiration time.
+    This token is used for user authentication in the application.
+    """
     access_token_payload = {
         "user_id": str(user_id),
         "exp": datetime.timestamp(datetime.now() + timedelta(minutes=5)),
@@ -16,6 +25,10 @@ def generate_access_token(user_id):
 
 
 def generate_refresh_token(user_id):
+    """
+    Generates a JWT refresh token with a longer expiration time.
+    This token can be used to renew the access token once it expires.
+    """
     refresh_token_payload = {
         "user_id": str(user_id),
         "exp": datetime.timestamp(datetime.now() + timedelta(minutes=30)),
@@ -27,5 +40,8 @@ def generate_refresh_token(user_id):
 
 
 def decode_refresh_token(refresh_token):
+    """
+    Decodes a given refresh token to extract its payload, such as the user ID and expiration timestamp.
+    """
     payload = jwt.decode(refresh_token, settings.SECRET_KEY, algorithms=["HS256"])
     return payload
