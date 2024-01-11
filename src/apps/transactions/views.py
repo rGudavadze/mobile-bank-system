0d0b09a2fb1f9b32f748c0001f7b9e72e6df90cc
TATO <tatososelia93@gmail.com>
2024-01-11 15:19:27 +0400

    feat(MBA-7) add viws for create, list and retrive transactions

--- src/apps/transactions/views.py
+++ src/apps/transactions/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+"""Views for the transactions app."""
+from rest_framework import permissions
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView
 
-# Create your views here.
+from apps.transactions.models import Transaction
+from apps.transactions.serializers import TransactionSerializer
+
+
+class CreateTransactionView(CreateAPIView):
+    """Create a new transaction."""
+    serializer_class = TransactionSerializer
+    queryset = Transaction.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
+
+class RetrieveTransactionView(RetrieveAPIView):
+    """Retrieve a transaction."""
+    serializer_class = TransactionSerializer
+    queryset = Transaction.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
+
+
+class ListTransactionView(ListAPIView):
+    """List all transactions."""
+    serializer_class = TransactionSerializer
+    queryset = Transaction.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
