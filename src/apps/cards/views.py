ec55e67411d91648acb2b1c9b77dfdd0e15a5078
Giorgi <giorgi.abashidze2@gmail.com>
2024-01-16 14:21:30 +0400

    feat(MBA-9): add views for cards

---
+++ src/apps/cards/views.py
@@ -0,0 +1,38 @@
+from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from utils.permissions import IsCardOwner
+
+from apps.cards.models import Account, Card
+from apps.cards.serializers import CardSerializer
+
+
+class CardListView(generics.ListCreateAPIView):
+    serializer_class = CardSerializer
+    permission_classes = [IsAuthenticated, IsCardOwner]
+
+    def get_queryset(self):
+        account_id = self.request.GET.get("account")
+
+        if account_id is not None:
+            queryset = Card.objects.filter(account__id=account_id)
+        else:
+            queryset = Card.objects.filter(account__profile__user=self.request.user)
+
+        return queryset
+
+    def create(self, request, *args, **kwargs):
+        account_id = request.data.get("account")
+        if not Account.objects.filter(id=account_id, profile__user=request.user).exists():
+            return Response(
+                {"error": "Invalid account for the current user."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        return super().create(request, *args, **kwargs)
+
+
+class CardDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Card.objects.all()
+    serializer_class = CardSerializer
+    permission_classes = [IsAuthenticated, IsCardOwner]
