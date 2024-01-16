073f877216a1be2929fb8b3858ee6746141b40fd
Giorgi <giorgi.abashidze2@gmail.com>
2024-01-16 14:49:28 +0400

    feat(MBA-9): change core/urls for cards

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from django.urls import include, path
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from django.urls import path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+)
+
 
 urlpatterns = [
     # Admin Site URLs
@@ -10,9 +14,9 @@ urlpatterns = [
     # API Schema URLs
     path("api/schema/", SpectacularAPIView.as_view(), name="api-schema"),
     path(
-        "api/docs/",
-        SpectacularSwaggerView.as_view(url_name="api-schema"),
-        name="api-docs",
+        'api/docs/',
+        SpectacularSwaggerView.as_view(url_name='api-schema'),
+        name='api-docs',
     ),
     path("api/", include("apps.cards.urls")),
 ]
