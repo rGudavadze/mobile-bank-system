63ada7437aa0913ec281f4af75764fc811d7a263
TATO <tatososelia93@gmail.com>
2024-01-16 17:08:12 +0400

    fix(MBA-7): fix endpoint view and url for transaction api

--- src/apps/transactions/urls.py
+++ src/apps/transactions/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
-from apps.transactions.views import CreateTransactionView, RetrieveTransactionView, ListTransactionView
+
+from apps.transactions.views import (
+    CreateTransactionView,
+    RetrieveTransactionView,
+)
 
 urlpatterns = [
-    path('transactions/', CreateTransactionView.as_view(), name='create_transaction',),
-    path('transactions/<uuid:id>', RetrieveTransactionView.as_view(), name='get_transaction', ),
-    path('transactions/', ListTransactionView.as_view(), name='list_transaction', ),
-
+    path(
+        "transactions/",
+        CreateTransactionView.as_view(),
+        name="list_create_transaction",
+    ),
+    path(
+        "transactions/<uuid:id>",
+        RetrieveTransactionView.as_view(),
+        name="get_transaction",
+    ),
 ]
--- src/apps/transactions/views.py
+++ src/apps/transactions/views.py
@@ -1,26 +1,36 @@
 """Views for the transactions app."""
 from rest_framework import permissions
-from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveAPIView,
+    get_object_or_404,
+)
 
 from apps.transactions.models import Transaction
 from apps.transactions.serializers import TransactionSerializer
 
 
-class CreateTransactionView(CreateAPIView):
+class CreateTransactionView(ListCreateAPIView):
     """Create a new transaction."""
+
     serializer_class = TransactionSerializer
     queryset = Transaction.objects.all()
     permission_classes = (permissions.IsAuthenticated,)
+
+    def get_queryset(self):
+        user = self.request.user
+        return Transaction.objects.filter(sender__profile__user=user)
+
 
 class RetrieveTransactionView(RetrieveAPIView):
     """Retrieve a transaction."""
+
     serializer_class = TransactionSerializer
-    queryset = Transaction.objects.all()
     permission_classes = (permissions.IsAuthenticated,)
 
+    def get_queryset(self):
+        return Transaction.objects.all()  # You can adjust this if needed
 
-class ListTransactionView(ListAPIView):
-    """List all transactions."""
-    serializer_class = TransactionSerializer
-    queryset = Transaction.objects.all()
-    permission_classes = (permissions.IsAuthenticated,)
+    def get_object(self):
+        transaction_id = self.kwargs.get("id")
+        return get_object_or_404(Transaction, id=transaction_id)
