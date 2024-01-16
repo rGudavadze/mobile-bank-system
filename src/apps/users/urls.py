8384b1587088ab1717c34b6bbd1d54f8215dbac4
Qildeli <anaqildeli@gmail.com>
2024-01-16 19:49:13 +0400

    refactor(MBA-6): Update views and URL paths names

--- src/apps/users/urls.py
+++ src/apps/users/urls.py
@@ -1,20 +1,20 @@
 from django.urls import path
 
-from apps.users import views
 from apps.users.views import (
-    PasswordForgetView,
-    PasswordResetView,
-    TokenObtainView,
-    TokenRefreshView,
+    PasswordForgetAPIView,
+    PasswordResetAPIView,
+    TokenRefreshAPIView,
+    UserLoginAPIView,
+    UserRegisterAPIView,
 )
 
 urlpatterns = [
     # Endpoint for user registration
-    path("register/", views.UserRegisterAPI.as_view(), name="user-register"),
-    # Endpoints for JWT token management
-    path("login/", TokenObtainView.as_view(), name="token-obtain"),
-    path("token-refresh/", TokenRefreshView.as_view(), name="token-refresh"),
+    path("register/", UserRegisterAPIView.as_view(), name="user-register"),
+    # Endpoints for user login and JWT token management
+    path("login/", UserLoginAPIView.as_view(), name="user-login"),
+    path("token-refresh/", TokenRefreshAPIView.as_view(), name="token-refresh"),
     # Password management endpoints
-    path("password-forget/", PasswordForgetView.as_view(), name="password-forget"),
-    path("password-reset/", PasswordResetView.as_view(), name="password-reset"),
+    path("password-forget/", PasswordForgetAPIView.as_view(), name="password-forget"),
+    path("password-reset/", PasswordResetAPIView.as_view(), name="password-reset"),
 ]
