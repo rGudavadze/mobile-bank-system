7caf7f95eb97b20d1df3dada8b9d5a424c8aca33
Qildeli <anaqildeli@gmail.com>
2024-01-18 17:29:51 +0400

    feat(MBA-10): Add test to check registration with existing email

--- src/apps/users/tests/test_register.py
+++ src/apps/users/tests/test_register.py
@@ -11,9 +11,9 @@ from apps.users.factories import UserFactory
 
 class UserRegisterTestCase(APITestCase):
     def setUp(self):
-        self.user = UserFactory()
+        self.user = UserFactory.create(email="userexists@gmail.com")
         self.url = reverse("user-register")
-        self.body = {"email": "unique_email@example.com", "password": "password"}
+        self.body = {"email": "unique_email@gmail.com", "password": "password"}
 
     def test_register_correct(self):
         """
@@ -24,11 +24,25 @@ class UserRegisterTestCase(APITestCase):
             self.url,
             self.body,
         )
-        # Asserting that the response status code is 201 (created)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         # Asserting that the response data contains the correct email
-        self.assertEqual(response.data.get("email"), "unique_email@example.com")
+        self.assertEqual(response.data.get("email"), "unique_email@gmail.com")
 
         # Check if user actually created in database
         user = get_user_model()
         self.assertTrue(user.objects.filter(email=self.body.get("email")).exists())
+
+    def test_register_existing_email(self):
+        """
+        Test that checking if the email already exists.
+        """
+        self.body.update(email="userexists@gmail.com")
+        response = self.client.post(
+            self.url,
+            data=self.body,
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        # Asserting that the first error message is the expected error message
+        self.assertEqual(
+            response.data.get("email")[0], "user with this email already exists."
+        )
