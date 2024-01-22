d02f0702da5ce691721d2a1ae12c404d28a32ad0
NikaMdinaradze <mdinaradzenika04@gmail.com>
2024-01-22 19:06:53 +0400

    Fix users access on others accounts

--- src/apps/account/views.py
+++ src/apps/account/views.py
@@ -1,14 +1,31 @@
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import generics
+from rest_framework.serializers import ValidationError
 
-from .models import Account
-from .serializers import AccountSerializer
+from apps.account.models import Account
+from apps.account.permissions import IsAccountOwner
+from apps.account.serializers import AccountSerializer
+from apps.profiles.models import Profile
 
 
 class AccountList(generics.ListCreateAPIView):
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
 
+    def get_queryset(self):
+        user = self.request.user
+
+        return Account.objects.filter(profile__user=user)
+
+    def perform_create(self, serializer):
+        try:
+            profile = Profile.objects.get(user=self.request.user)
+        except ObjectDoesNotExist:
+            raise ValidationError(code=404, detail="User Profile Does Not Exist")
+        serializer.save(profile=profile)
+
 
 class AccountDetail(generics.RetrieveDestroyAPIView):
+    permission_classes = [IsAccountOwner]
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
