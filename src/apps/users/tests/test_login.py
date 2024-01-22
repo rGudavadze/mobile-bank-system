b3758202fc91c761e0ace61f78e49dacb1427fe5
Qildeli <anaqildeli@gmail.com>
2024-01-22 18:40:28 +0400

    feat(MBA-10): Add test for login with incorrect email and password

--- src/apps/users/tests/test_login.py
+++ src/apps/users/tests/test_login.py
@@ -15,9 +15,27 @@ class UserLoginTestCase(APITestCase):
         self.url = reverse("user-login")
         self.body = dict(email="email@gmail.com", password="password")
 
-    def test_successful_login(self):
+    def test_login_successful(self):
         """
         Test successful login.
         """
         response = self.client.post(self.url, self.body)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_login_incorrect_email(self):
+        """
+        Test login with incorrect email address.
+        """
+        self.body.update(email="incorrect@gmail.com")
+        response = self.client.post(self.url, self.body)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data.get("non_field_errors")[0], "Unable to log in.")
+
+    def test_login_incorrect_password(self):
+        """
+        Test login with incorrect password.
+        """
+        self.body.update(password="incorrect_password")
+        response = self.client.post(self.url, self.body)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data.get("non_field_errors")[0], "Unable to log in.")
