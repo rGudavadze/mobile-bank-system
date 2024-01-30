74c0fdf8eec20bfd306e7f24a8d91ca486d18bdc
Qildeli <anaqildeli@gmail.com>
2024-01-30 15:52:15 +0400

    feat(MBA-10): Add test for generate_refresh_token method

--- src/apps/users/tests/test_jwt_utils.py
+++ src/apps/users/tests/test_jwt_utils.py
@@ -9,7 +9,7 @@ from django.conf import settings
 from rest_framework.test import APITestCase
 
 from apps.users.factories import UserFactory
-from apps.users.jwt_utils import generate_access_token
+from apps.users.jwt_utils import generate_access_token, generate_refresh_token
 
 
 class JWTUtilsTestCase(APITestCase):
@@ -24,8 +24,10 @@ class JWTUtilsTestCase(APITestCase):
         Test the JWT access token successful generation.
         """
 
-        mock_jwt_encode.return_value = "mocked_token"
-        exp = mock_datetime.timestamp.return_value = self.fixed_now + timedelta(minutes=5)
+        mock_jwt_encode.return_value = "mocked_access_token"
+        exp = mock_datetime.timestamp.return_value = self.fixed_now + timedelta(
+            minutes=60
+        )
 
         access_token = generate_access_token(self.user.id)
 
@@ -34,4 +36,23 @@ class JWTUtilsTestCase(APITestCase):
         jwt.encode.assert_called_once_with(
             access_token_payload, settings.SECRET_KEY, algorithm="HS256"
         )
-        self.assertEqual(access_token, "mocked_token")
+        self.assertEqual(access_token, "mocked_access_token")
+
+    @patch("apps.users.jwt_utils.jwt.encode")
+    @patch("apps.users.jwt_utils.datetime")
+    def test_generate_refresh_token_success(self, mock_datetime, mock_jwt_encode):
+        """
+        Test the JWT refresh token successful generation.
+        """
+
+        mock_jwt_encode.return_value = "mocked_refresh_token"
+        exp = mock_datetime.timestamp.return_value = self.fixed_now + timedelta(days=30)
+
+        refresh_token = generate_refresh_token(self.user.id)
+
+        refresh_token_payload = {"user_id": str(self.user.id), "exp": exp}
+
+        jwt.encode.assert_called_once_with(
+            refresh_token_payload, settings.SECRET_KEY, algorithm="HS256"
+        )
+        self.assertEqual(refresh_token, "mocked_refresh_token")
